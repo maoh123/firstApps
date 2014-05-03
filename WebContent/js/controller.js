d4ab8fe5f4dd61e4cf2a399bcc9cedd54648d08d
maoh123 <maoh_123@hotmail.com>
2014-05-04 01:14:27 +0200

    Ready for coding
    Minimal code for implementation

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -1,12 +1,27 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('FirstAppController', ['$scope', function($scope) {
+	/*
+	Some constants
+	*/
+	var RED  = 'red';
+	var BLUE = 'blue';
+	var FREE = 'free';
+
     /*
 	Make a movement ... or not
 	*/
-	$scope.move = function($toBeMoved) {
-		var id = $toBeMoved.css('color');
-        alert("Trying moving " + id);
+	$scope.move = function(event) {
+		var color = $(event.target).attr('class');
+		var id    = $(event.target).attr('id');
+		$scope.debug = 'Trying to move ' + color + ' ' + id ;
+		if (isFree(color)) {
+			// Movement not OK
+			$scope.feedback = 'Movement not possible';
+		} else {
+			// Movement OK
+			$scope.feedback = '';
+		}
     };
 
 	/*
@@ -14,5 +29,26 @@ myApp.controller('FirstAppController', ['$scope', function($scope) {
 	*/
     $scope.reset = function() {
         //TO DE DONE
+		$scope.debug = '';
+		$scope.feedback = 'Reset done';
+    };
+	
+	/*
+	Test if case is red
+	*/
+    isRed = function(color) {
+		return (color == RED);
+    };
+	/*
+	Test if case is blue
+	*/
+    isBlue = function(color) {
+		return (color == BLUE);
+    };
+	/*
+	Test if case is free
+	*/
+   isFree = function(color) {
+		return (color == FREE);
     };
 }]);
